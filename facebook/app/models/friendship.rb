2aff601ae7cba32860721c0ee3266200ba9fe47a
Rodolfo Llinás <rllinasc@unal.edu.co>
2020-02-04 07:39:01 -0500

    fixing views

--- facebook/app/models/friendship.rb
+++ facebook/app/models/friendship.rb
@@ -2,7 +2,7 @@
 
 class Friendship < ApplicationRecord
   belongs_to :user
-  belongs_to :friend, class_name: :user
+  belongs_to :friend, class_name: 'User'
 
   validates :user, uniqueness: { scope: :friend }
   validate :user_and_friend_are_different
