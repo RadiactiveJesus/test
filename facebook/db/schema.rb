937953bcdd5d174b7c4d265d061195dc794280d0
Rodolfo Llinás <rllinasc@unal.edu.co>
2020-02-06 11:14:04 -0500

    solving migration issues

--- facebook/db/schema.rb
+++ facebook/db/schema.rb
@@ -17,11 +17,11 @@ ActiveRecord::Schema.define(version: 2020_01_23_091033) do
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "post_id"
     t.string "commentary"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.bigint "user_id"
-    t.bigint "post_id"
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
   end
